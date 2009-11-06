4051dd07f750f641a7cffd126c8ef680003be1a4
Scott Cowan <info@scottcowan.org>
2009-11-06 13:55:14 +0000

    removing custom implementations of Logger,PatternLayout to use MDC hashtable instead

--- log4j/CustomFields/src/logging/Main.java
+++ log4j/CustomFields/src/logging/Main.java
@@ -1,40 +1,31 @@
 package logging;
 
-import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
+import org.apache.log4j.MDC;
 import org.apache.log4j.PropertyConfigurator;
 
 public class Main {
-   private static String formatString =
-      "----------------using in code appender----------------------%n" +
-      "Time:      %d%n" +
-      "Host:      %h%n" +
-      "Server:    %s%n" +
-      "Component: %b%n" +
-      "Version:   %v%n" +
-      "Priority:  %p%n" +
-      "Thread Id: %t%n" +
-      "Context:   %x%n" +
-      "Message:   %m%n";
 
     public static void main(String[] args) {
         PropertyConfigurator.configure("customlog.properties");
-        AppServerLoggerFactory factory;
-        factory = new AppServerLoggerFactory("MyServer", "MyComponent", "1.0");
-        AppServerLogger.setFactory(factory);
-        //with_appender().info("logging config from code");
-        without_appender().info("logging config from customlog.properties");
+        setLogContext("MyServer", "MyComponent", "1.0");
+        Logger log = LogManager.getLogger("some.log");        
+        log.info("Hello");
     }
 
-    public static Logger with_appender(){
-        Logger logger = AppServerLogger.getLogger("some.log");
-        PatternLayout layout = new AppServerPatternLayout( formatString );
-        logger.addAppender( new ConsoleAppender(layout) );
-        return logger;
-    }
+    public static void setLogContext(String serverName,String componentName, String versionName){
+     try
+      {
+         MDC.put("Host", java.net.InetAddress.getLocalHost().getHostName());
+      }
+      catch ( java.net.UnknownHostException uhe )
+      {
+         System.err.println("Could not determine local hostname.");
+      }
 
-    public static Logger without_appender(){
-        return AppServerLogger.getLogger("some.log");
+      MDC.put("Server", serverName);
+      MDC.put("Component", componentName);
+      MDC.put("Version", versionName);
     }
 }
